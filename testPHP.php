82d6d0c17577ba4bda2940026ad2eb2042454680
SonGoK4RiO <96130836+SonGoK4RiO@users.noreply.github.com>
2021-12-14 16:10:06 +0100

    initial commit
    salut github

---
+++ testPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Test PHP</title>
+    </head>
+
+
+    <body>
+        <p>un bout de code en HTML</p>
+        <?php
+        echo 'Mon premier script en PHP';
+        ?>
+        </body>
+</html>
